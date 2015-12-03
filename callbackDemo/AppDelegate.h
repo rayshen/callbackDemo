00595fb93bf7071de2bfa2a3eaa0cee9ca49fd8c
rayshen <rayshen1012@gmail.com>
2015-12-03 13:10:43 +0800

    Initial Commit

---
+++ callbackDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  callbackDemo
+//
+//  Created by shen on 15/12/3.
+//  Copyright © 2015年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
